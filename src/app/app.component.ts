b85e629cd52e3467b9a45c94edd8c5f655a58773
Matheus Melo <teteretu@yahoo.com>
2023-04-22 15:44:32 -0300

    configuring service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pythonTutorialFront';
+export class AppComponent implements OnInit {
+    public message: string = "";
+
+    constructor(private apiService: ApiService) { }
+
+    public ngOnInit() {
+        this.apiService.getHello().subscribe((data: any) => {
+            this.message = data.hello;
+        });
+    }
 }
